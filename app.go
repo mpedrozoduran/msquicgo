fef1b11b0a4b3b093fd136af36f9eacceb8cdabd
Miguel Pedrozo <mpedrozoduran@gmail.com>
2021-03-28 16:51:38 -0500

    fixed CGO errors

--- app.go
+++ app.go
@@ -4,18 +4,29 @@ package main
 #cgo CFLAGS: -I/home/miguel/samples/msquic/src/inc
 #cgo LDFLAGS: -L/home/miguel/samples/msquic/build/bin/Release -l:libmsquic.so
 #include "msquic.h"
+#include "stdlib.h"
+const QUIC_REGISTRATION_CONFIG RegConfig = { "quicsample", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+HQUIC Registration;
+const QUIC_API_TABLE* MsQuic;
+
+int call_msquic_reg_open(HQUIC* reg, QUIC_REGISTRATION_CONFIG* config) {
+	return MsQuic->RegistrationOpen(config, reg);
+}
 */
 import "C"
-import "log"
+import (
+	"log"
+	"unsafe"
+)
 
 type Quic struct {
 	APITable  *C.struct_QUIC_API_TABLE
 	RegConfig C.struct_QUIC_REGISTRATION_CONFIG
-	Config    C.struct_QUIC_HANDLE
+	Config    *C.HQUIC
 }
 
 func NewQuic() Quic {
-	quic := Quic{}
+	quic := Quic{RegConfig: C.RegConfig, Config: &C.Registration}
 	quic.open()
 	quic.registration()
 	return quic
@@ -35,11 +46,9 @@ func (m *Quic) open() {
 }
 
 func (m *Quic) registration() {
-	regConfig := C.struct_QUIC_REGISTRATION_CONFIG{"myquicsample", C.QUIC_EXECUTION_PROFILE_LOW_LATENCY}
-	config := C.struct_QUIC_HANDLE
-	m.Config = config
-	m.RegConfig = regConfig
-	m.APITable.RegistrationOpen(&regConfig, &config)
+	cs := C.CString("myquicsample")
+	C.call_msquic_reg_open(m.Config, &m.RegConfig)
+	C.free(unsafe.Pointer(cs))
 }
 
 func (m *Quic) runServer() {
@@ -51,5 +60,5 @@ func (m *Quic) runClient() {
 }
 
 func (m *Quic) free() {
-	C.MSQuicClose(&m.APITable)
+	C.MsQuicClose(m.APITable)
 }
